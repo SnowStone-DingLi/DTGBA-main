e7ad8a588e686e64ef3bd0c317307c2b7739d689
Ding Li <liding1583@stu.ouc.edu.cn>
2025-05-06 10:04:41 +0800

    Update run_DTGBA_under_RandomEdgeDropping.py

--- run_DTGBA_under_RandomEdgeDropping.py
+++ run_DTGBA_under_RandomEdgeDropping.py
@@ -4,7 +4,7 @@ import argparse
 import numpy as np
 import torch
 from copy import deepcopy
-from torch_geometric.datasets import Planetoid, Reddit2, Flickr, CitationFull, Amazon, Twitch, Actor
+from torch_geometric.datasets import Planetoid, Reddit2, Flickr, CitationFull, Amazon, Twitch, Actor, WikipediaNetwork
 
 # from torch_geometric.loader import DataLoader
 from help_funcs import prune_unrelated_edge, prune_unrelated_edge_isolated, get_rand_edge_dropping
@@ -22,7 +22,7 @@ parser.add_argument('--model', type=str, default='GCN', help='model',
                     choices=['GCN', 'GAT', 'GraphSage', 'GIN'])
 parser.add_argument('--dataset', type=str, default='ogbn-arxiv',
                     help='Dataset',
-                    choices=['Cora', 'Pubmed', 'ogbn-arxiv'])
+                    choices=['Cora', 'Pubmed', 'ogbn-arxiv',  'Actor', 'chameleon'])
 parser.add_argument('--train_lr', type=float, default=0.01,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4,
@@ -145,6 +145,12 @@ elif (args.dataset in ['DE', 'EN','ES', 'FR', 'PT' , 'RU']):
 elif (args.dataset == 'Actor'):
     dataset = Actor(root='./data/', \
                         transform=transform)
+elif (args.dataset in ['chameleon', 'crocodile','squirrel']):
+    dataset = WikipediaNetwork(root='./data/', \
+                        name=args.dataset, \
+                        geom_gcn_preprocess=True, \
+                        transform=transform)
+    
 data = dataset[0].to(device)
 
 if (args.dataset == 'ogbn-arxiv'):
@@ -168,6 +174,12 @@ elif (args.dataset == 'Computers' or args.dataset == 'Photo' or args.dataset == 
     data.val_mask = torch.zeros(nNode, dtype=torch.bool).to(device)
     data.test_mask = torch.zeros(nNode, dtype=torch.bool).to(device)
 elif (args.dataset in ['DE', 'EN','ES', 'FR', 'PT' , 'RU']):
+    nNode = data.x.shape[0]
+    setattr(data, 'train_mask', torch.zeros(nNode, dtype=torch.bool).to(device))
+    # dataset[0].train_mask = torch.zeros(nEdge, dtype=torch.bool).to(device)
+    data.val_mask = torch.zeros(nNode, dtype=torch.bool).to(device)
+    data.test_mask = torch.zeros(nNode, dtype=torch.bool).to(device)
+elif (args.dataset in ['chameleon', 'crocodile','squirrel']):
     nNode = data.x.shape[0]
     setattr(data, 'train_mask', torch.zeros(nNode, dtype=torch.bool).to(device))
     # dataset[0].train_mask = torch.zeros(nEdge, dtype=torch.bool).to(device)
